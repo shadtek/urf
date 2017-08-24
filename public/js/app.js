6fa8be3aaca1c4ad918b9042d72cd85d65cbaf20
Spencer Shattuck <spencer.shattuck@gmail.com>
2017-08-23 23:31:41 -0600

    Add Angular 1.6.6

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('utahrenfaire', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+		.state('home', {
+			url: "/",
+			templateUrl: "views/home/home.html",
+			controller: 'homeCtrl'
+		})
+		.state('otherwise', {
+			url: '*path',
+			resolve: {
+				redirect: function($location) {
+					$location.path("/").replace()
+					console.log("redirecting...")
+				}
+			}
+		});
+
+		$locationProvider.html5Mode(true);
+
+})
